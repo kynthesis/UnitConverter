c8a2423772f7d89e3acc13195039af0bdc47d181
Khoa D. Vo <khoa.synthesis@gmail.com>
2024-01-09 15:15:16 +0700

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Khoa D. Vo on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
